2614eebc0579f7ef04026d891c5b769341ae65ed
Bob Tate <btate712@gmail.com>
2020-11-11 13:00:41 -0600

    completed exercise 4.17

---
+++ ch4/ex4.17/MilageCalculator.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MilageCalculator {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int totalMiles = 0;
+    int totalGallons = 0;
+    boolean keepGoing = true;
+
+    while(keepGoing) {
+      System.out.printf("Please enter an integer representation of the miles driven for a trip: ");
+      totalMiles += input.nextInt();
+      
+      System.out.printf("Please enter an integer representation of gallons used for the trip: ");
+      totalGallons += input.nextInt();
+
+      System.out.print("Would you like to enter another trip? (y/n):");
+      keepGoing = input.next().charAt(0) == 'y' ? true : false;
+    }
+
+
+    System.out.printf("You've travelled a total of %d miles and used a total of %d gallons. Your average gas mileage was %.2f mpg.\n", totalMiles, totalGallons, (float)totalMiles/(float)totalGallons);
+    
+
+
+  }
+}
